77c88d6160a1e77a341967c7c9b41a8c236a51be
Rodrigo-171 <rodrigonavarro171@gmail.com>
2022-07-01 18:24:22 -0300

    Primeiro Commit

---
+++ js/video-selector.js
@@ -0,0 +1,11 @@
+function trocar1()  {
+    document.getElementById("video").src="media/HTML 5 - Videos e IFrames em HTML (Video 09).mp4";
+}
+
+function trocar2() {
+    document.getElementById("video").src="media/JavaScript - Como usar JavaScript trocar um conteúdo imagem.mp4";
+}
+
+function trocar3() {
+    document.getElementById("video").src="https://www.youtube.com/embed/WsEv01p3GXU";
+}
